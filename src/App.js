edd7391b382df154e68774ab49d7c2575f3be277
myaooooo98 <yaoer0505@gmail.com>
2023-08-15 23:23:54 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Intro from "./components/Intro"
+
+export default function App() {
+    return (
+        <div>
+            <Intro />
+        </div>
+    )
+}
---
+++ src/components/Intro.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Intro() {
+    return (
+        <div className="intro-container">
+            <h1 className="title">Quizzical</h1>
+            <p className="description">Take quizzes to test your knowledge on a variety of fun and interesting topics!</p>
+            <button className="intro-btn">Start Quiz</button>
+        </div>
+    )
+}
