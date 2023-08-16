1f7e20a699790f0bbf88f557fa710dc52acc76a3
myaooooo98 <yaoer0505@gmail.com>
2023-08-16 22:39:40 +0800

    add background

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Intro from "./components/Intro"
 export default function App() {
     return (
         <div>
+            <img src="./images/blobs (1).png" className="blob1"></img>
             <Intro />
+            <img src='./images/blobs.png' className="blob2"></img>
         </div>
     )
 }
---
+++ src/components/Quiz.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function Quiz() {
+    return (
+        <h3 className="question"></h3>
+    )
+}
