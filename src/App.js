744d46d9b1a904653f1091ecbaee1a3766af7a8b
myaooooo98 <yaoer0505@gmail.com>
2023-08-18 23:54:43 +0800

    add startQuiz function and state

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React from "react"
 import Intro from "./components/Intro"
 
 export default function App() {
+    const [isStartQuiz, setIsStartQuiz] = React.useState(false)
+
     function startQuiz() {
-        const styles = {
-            display: 'none'
-        }
-        return styles
+        setIsStartQuiz(true)
     }
 
     return (
         <div>
             <img src="./images/blobs (1).png" className="blob1"></img>
-            <Intro 
-                handleClick = {startQuiz}
+            <Intro
+                isStartQuiz = {isStartQuiz} 
+                startQuiz = {startQuiz}
             />
             <img src='./images/blobs.png' className="blob2"></img>
         </div>
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,13 +1,16 @@
 import React from "react"
 
 export default function Intro(props) {
+
+    let containerClass = `intro-container ${props.isStartQuiz ? 'start' : ''}`
+
     return (
-        <div className="intro-container">
+        <div className={containerClass}>
             <h1 className="title">Quizzical</h1>
             <p className="description">Take quizzes to test your knowledge on a variety of fun and interesting topics!</p>
             <button 
                 className="intro-btn"
-                onClick={props.handleClick}
+                onClick={props.startQuiz}
             >
                     Start Quiz
             </button>
